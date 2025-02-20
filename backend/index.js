875934eeb9fae5aa983cb205bc89a092f14a4a80
ayush.kumar <ayush.kumar@esoftech.com>
2025-02-20 12:27:29 +0530

    added frontend

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,6 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: 'Internal Server Error' });
   });
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
